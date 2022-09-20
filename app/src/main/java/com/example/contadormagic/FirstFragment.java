348bc353b008584e0943a829a44ff285d3710c4f
Usuario <vicen.ll.d@gmail.com>
2022-09-20 23:35:21 +0200

    Intregacion de bottones y algunas funciones

--- app/src/main/java/com/example/contadormagic/FirstFragment.java
+++ app/src/main/java/com/example/contadormagic/FirstFragment.java
@@ -14,6 +14,10 @@ import com.example.contadormagic.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    int lp1= 0;
+    int lp2= 0;
+    int pp1= 0;
+    int pp2= 0;
 
     @Override
     public View onCreateView(
@@ -22,20 +26,60 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        reset();
+        if(savedInstanceState!=null){
+            lp1= savedInstanceState.getInt("lp1");
+            lp2= savedInstanceState.getInt("lp2");
+            pp1= savedInstanceState.getInt("pp1");
+            pp2= savedInstanceState.getInt("pp2");
+
+        }
+        updateCounters();
+
         return binding.getRoot();
 
+    }
+
+    private void updateCounters() {
+        binding.TXTLP1.setText(lp1+"/"+pp1);
+
+        binding.TXTLP2.setText(lp2+"/"+pp2);
+    }
+
+    private void reset() {
+         lp1= 20;
+         lp2= 20;
+         pp1= 0;
+         pp2= 0;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
+        binding.btnAP1.setOnClickListener(btn -> {
+            lp1++;
+            lp2--;
+                }
+        );
+        binding.btnAP2.setOnClickListener(bnt ->{
+            lp2++;
+            lp1--;
         });
+
+        binding.btnLlP1.setOnClickListener(btn -> {
+                lp1--;
+                }
+        );
+
+        binding.BtnPlP1.setOnClickListener(btn ->{
+            pp1--;
+        });
+
+        binding.btnPpP1.setOnClickListener(btn ->{
+            pp1++;
+        });
+
     }
 
     @Override
